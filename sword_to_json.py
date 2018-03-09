2b0b6da6f0cebdb297d85b42698572750148746c
laurajauch <laurajauch@users.noreply.github.com>
2018-03-08 20:13:53 -0500

    py 2 AND 3 support
    Now it works for both!

--- sword_to_json.py
+++ sword_to_json.py
@@ -1,4 +1,5 @@
 from pysword.modules import SwordModules
+from past.builtins import xrange
 import argparse, json
 
 def generate_dict(source_file, bible_version):
@@ -13,9 +14,9 @@ def generate_dict(source_file, bible_version):
 
     for book in books:
         chapters = []
-        for chapter in range(1, book.num_chapters+1):
+        for chapter in xrange(1, book.num_chapters+1):
             verses = []
-            for verse in range(1, len(book.get_indicies(chapter))+1 ):
+            for verse in xrange(1, len(book.get_indicies(chapter))+1 ):
                 verses.append({
                     'verse': verse,
                     'chapter': chapter,
